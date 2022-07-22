5691228b5a6a9d446bbba9c68a8ef9d8f46ccdd0
tslumley <t.lumley@auckland.ac.nz>
2022-07-22 14:36:08 +1200

    data extension

--- R/rata.R
+++ R/rata.R
@@ -6,28 +6,45 @@ hasany<-function(mr_object, value) mr_object %hasany% value
 hasall<-function(mr_object, value) mr_object %hasall% value
 
 mrglm<-function(formula, data, family,...){
- 	
+     g<-match.call()
+	
     special <- c("long")
     tform <- if (missing(data)) 
         terms(formula, special)
     else terms(formula, special, data = data)
     
-    if (!(all(all.vars(formula) %in% names(data))))
-	    stop("all variables must be in data= argument")
-
-	if (!is.null(attr(tform,"specials")$long)){
-		## need to expand the data
-		whichlong<-attr(tform,"specials")$long
-		mf<-model.frame(formula, data)
-		nms<-sapply(names(mf)[whichlong],as.name)
-		longmr<- eval(bquote(with(mf, mr_stack(..(nms))), splice=TRUE))
-		longmf<-mf[longmr$id,]
-		longmf[,whichlong]<-longmr[,-1]
-	}
-
-
-
+     if (!(all(all.vars(formula) %in% names(data))))
+         stop("all variables must be in data= argument")
+     
+     if (!is.null(attr(tform,"specials")$long)){
+         ## need to expand the data
+         whichlong<-attr(tform,"specials")$long
+         mf<-model.frame(formula, data)
+         nms<-sapply(names(mf)[whichlong],as.name)
+         names(nms)<-NULL
+         longmr<- eval(bquote(with(mf, mr_stack(..(nms))), splice=TRUE))
+         longmf<-mf[longmr$id,]
+         longmf[,whichlong]<-longmr[,!(names(longmr) %in% "id")]
+         g$data<-as.name("longmf")
+         id<-longmr$id
+     } else {
+         id<-NULL
+     }
+     g[[1]]<-as.name("glm")
+     rval<-eval(g)
+     infl<-mrglm_estfun(rval)%*%summary(rval)$cov.unscaled
+     
+     if (!is.null(id))
+         V<-crossprod(rowsum(infl, longmr$id))
+     else
+         V<-crossprod(infl)
+     
+     rval$naive.cov<-summary(rval)$cov.scaled
+     rval$vcov<-V
+     class(rval)<-c("mrglm","glm")
+     rval
 }
 
+vcov.mrglm<-function(model,...) model$vcov
 
 mrglm_estfun<-function(model) model.matrix(model)*model$weights*resid(model,"working")
